5beebfcdfebfd94c92c97acbbb6c551cfdd06601
Armaan Khan <kh.armaan535@gmail.com>
2024-11-10 00:39:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // Function to show the modal with the clicked image
 function showImage(imgElement) {
-    // Stop the rotation of the gallery by adding a "no-rotate" class
-    document.querySelector('.gallery').style.animationPlayState = 'paused';
-  
-    // Set the image in the modal
-    const modal = document.getElementById('imageModal');
-    const modalImage = document.getElementById('modalImage');
-    modal.style.display = "flex";
-    modalImage.src = imgElement.src;
-  }
-  
-  // Function to close the modal
-  function closeModal() {
-    // Resume the rotation of the gallery
-    document.querySelector('.gallery').style.animationPlayState = 'running';
-  
-    // Hide the modal
-    const modal = document.getElementById('imageModal');
-    modal.style.display = "none";
-  }
-  
+  // Stop the rotation of the gallery by adding a "no-rotate" class
+  document.querySelector('.gallery').style.animationPlayState = 'paused';
+
+  // Set the image in the modal
+  const modal = document.getElementById('imageModal');
+  const modalImage = document.getElementById('modalImage');
+  modal.style.display = "flex";
+  modalImage.src = imgElement.src;
+}
+
+// Function to close the modal
+function closeModal() {
+  // Resume the rotation of the gallery
+  document.querySelector('.gallery').style.animationPlayState = 'running';
+
+  // Hide the modal
+  const modal = document.getElementById('imageModal');
+  modal.style.display = "none";
+}
